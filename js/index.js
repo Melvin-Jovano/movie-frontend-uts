216402b430d1f2cc4f1d6498323cb79a9b95d260
Melvin-Jovano <melvinjovano2@gmail.com>
2022-11-08 06:48:26 +0700

    finalize list view

--- js/index.js
+++ js/index.js
@@ -1,31 +1,47 @@
+const movieContainer = document.getElementById('movie-container');
+
 async function getAllMovies() {
     try {
+        const loadWrapper = document.createElement('div');
+        loadWrapper.innerHTML = '<br>Loading... <br><br>';
+        movieContainer.appendChild(loadWrapper);  
+
         const getAllMovies = await axios.get(`http://localhost:3000/api/movies`);
+
         if(getAllMovies.data.message !== 'Success') throw Error();
-        const movieContainer = document.getElementById('movie-container');
+        
+        movieContainer.innerHTML = '';
 
         getAllMovies.data.data.forEach(movie => {
+            const a = document.createElement('a');
+            a.setAttribute('class', 'text-decoration-none');
+            a.href = `movie.html?id=${movie.id}`;
+
             const table = document.createElement('table');
             table.setAttribute('class', 'w-100');
-            // table.setAttribute('border', '1');
 
             const tr1 = document.createElement('tr');
             const tdImg = document.createElement('td');
-            tdImg.setAttribute('class', 'w-15 h-55px');
-            tdImg.setAttribute('rowspan', '2');
+            tdImg.setAttribute('class', 'w-15 h-60px');
+            tdImg.setAttribute('rowspan', '3');
+            tdImg.setAttribute('align', 'top');
             tdImg.innerHTML = `<img class="thumbnail" src="https://image.tmdb.org/t/p/w92${movie.poster_path}" alt="img" />`;
 
             const tdTitle = document.createElement('td');
             const div = document.createElement('div');
-            div.setAttribute('class', 'pl-2 title');
+            div.setAttribute('class', 'pl-15px title pt-10');
+            if(!movie.tagline) {
+                div.setAttribute('class', 'pl-15px title');
+                tdTitle.setAttribute('rowspan', '2');
+            }
             div.innerHTML = movie.title;
             tdTitle.appendChild(div);
 
             const tdReadMore = document.createElement('td');
-            tdReadMore.setAttribute('class', 'pr-1 w-10');
+            tdReadMore.setAttribute('class', 'pr-1 w-10 text-muted');
             tdReadMore.setAttribute('align', 'right');
             tdReadMore.setAttribute('rowspan', '2');
-            tdReadMore.innerHTML = '>';
+            tdReadMore.innerHTML = '&gt';
 
             tr1.appendChild(tdImg);
             tr1.appendChild(tdTitle);
@@ -34,14 +50,26 @@ async function getAllMovies() {
 
             if(movie.tagline) {
                 const tr2 = document.createElement('tr');
-                tr2.innerHTML = `<td class='pl-2 text-muted'><div class='title'><small>${movie.tagline}</small></div></td>`;
+                tr2.innerHTML = `<td class='pl-15px pb-10 text-muted'><div class='title'><small>${movie.tagline}</small></div></td>`;
                 table.appendChild(tr2);
             }
-            movieContainer.appendChild(table);
-        });
 
+            a.appendChild(table);
+            movieContainer.appendChild(a);
+
+            const hr = document.createElement('hr');
+            hr.setAttribute('class', 'm-0 p-0');
+            const hrWrapper = document.createElement('div');
+            hrWrapper.appendChild(hr);
+            hrWrapper.setAttribute('class', 'pl-75px');
+
+            movieContainer.appendChild(hrWrapper);
+        });
     } catch (error) {
-        console.error(error);        
+        movieContainer.innerHTML = '';
+        const errorWrapper = document.createElement('div');
+        errorWrapper.innerHTML = '<br>Maaf Coba Beberapa Saat Lagi... <br><br>';
+        movieContainer.appendChild(errorWrapper);   
     }
 }
 
